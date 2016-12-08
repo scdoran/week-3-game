699907e82821bef15832204abd73972250d0801c
Sarah Doran <sarahdoran@Sarahs-MacBook-Air-2.local>
2016-12-07 22:37:54 -0500

    Made a lot of changes to the code. Got the text to appear, but am having trouble getting the guessedLetters array to take in my eventkeys. Also working on replacing the words to dashes.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,77 +1,73 @@
+// Beginning of the game.
+
+// Object containing the game's properties for wins, losses and guesses left.
+	var game = {
+		wins: 0,
+		losses: 0,
+		guessesLeft: 7,
+	}
 
 // Word choices.
 var words = ["diane", "coffee", "lodge", "truman", "laura"];
 
-// Beginning of the game.
-var game = {
-	wins: 0,
-	losses: 0,
-	guessesLeft: 10,
+// This will make the words turn into dashes.
 
-	gameBegin: function() {
-		// Not sure whether or not to put the dash code here or somewhere else (see below in the onkeyup function() code.)
-		function wordChoice() {
+
+// Not sure whether or not to put the dash code here or somewhere else (see below in the onkeyup function() code.)
+	function wordChoice() {
 		return words[Math.floor(Math.random() * words.length)];
-		this.computerWord = this.computerWord.toLowerCase();
-		}
-		// Variable for the words chosen will be based off the function above.
-		var computerWord = wordChoice();
+	}
+// Variable for the words chosen will be based off the function above.
+	var computerWord = wordChoice().replace(/[A-Z]/g, "-");	
+	console.log(computerWord);
 
-		var guessedLetters = [];
+//This is the empty array that will hold any guessed letters. 
+	var guessedLetters = [];
 
-		function isInArray(a) {
-    	for (var i = 0;i < guessedLetters.length; i++) {
-        	if (guessedLetters[i] === a) {
-          	return true;
-        	}
-    	}
-    	return false;
-		}
-
-		document.getElementById("hangman-words").innerHTML = this.computerWord;
-		document.getElementById("guesses-left").innerHTML = game.guessesLeft;	
-	}	
-
-}
-
-// All of the stuff below will happen when a key is released!
+// // All of the stuff below will happen when a key is released!
 	document.onkeyup = function (event) {
 
 		var userGuess = event.key;
 
-// Still figuring out the conditionals here, so not much has changed from the psychic game.
-		if (userGuess === computerWord) {
-			document.getElementById("guesses").textContent = " ";
-			game.guessesLeft = 10;
-			var computerWord = wordChoice();
-			guessedLetters = [];
+		for (var i = 0; i < guessedLetters.length; i++) {
+			guessedLetters.push(userGuess);
 		}
+			console.log(guessedLetters);
 
-		else if (userGuess !== computerWord) {
-			if (this.isInArray(userGuess)) {
-          		alert("You already guessed that letter!");
-       		}
-
-       		else{
-				game.guessesLeft--;
-				guessedLetters.push(userGuess);
-				document.getElementById("guesses").textContent += event.key + " ";
-			}
-		}
-	
-	// If the user runs out of guesses the following will happen...
-		if (game.guessesLeft === 0) {
-			game.losses++;
-			document.getElementById("guesses").textContent=" ";
-			console.log("You lost. The word was " + this.computerWord);
-			document.getElementById("picture").src="../images/bob.gif"
-// Audio code that I found.
-			var audio = new Audio('../sound/fwwmpoem.wav');
-			audio.play();
-			// Guesses left are reset, computer chooses a new word, and the guessedLetters array becomes empty again. 
-			game.guessesLeft = 10;
-			var computerWord = wordChoice();
-			guessedLetters = [];
-		}
+		document.getElementById("guesses").textContent += event.key + " ";
 	}
+
+// 		if (userGuess === indexOf(searchString: string, position?: int)) {
+// 			document.getElementById("guesses").textContent = " ";
+// 			game.guessesLeft = 7;
+// 			var computerWord = wordChoice();
+// 			guessedLetters = [];
+// 		}
+
+// 		else if (userGuess !== computerWord) {
+// 			if (this.isInArray(userGuess)) {
+//        		}
+
+//        		else{
+// 				game.guessesLeft--;
+// 				guessedLetters.push(userGuess);
+// 				document.getElementById("guesses").textContent += event.key + " ";
+// 			}
+// 		}
+	
+// 	// If the user runs out of guesses the following will happen...
+// 		if (game.guessesLeft === 0) {
+// 			game.losses++;
+// 			document.getElementById("guesses").textContent=" ";
+// 			console.log("You lost. The word was " + this.computerWord);
+// 			document.getElementById("picture").src="../images/bob.gif"
+// // Audio code that I found.
+// 			var audio = new Audio('../sound/fwwmpoem.wav');
+// 			audio.play();
+// 			// Guesses left are reset, computer chooses a new word, and the guessedLetters array becomes empty again. 
+// 			game.guessesLeft = 7;
+// 			var computerWord = wordChoice();
+// 			guessedLetters = [];
+// 		}
+// 	}
 
