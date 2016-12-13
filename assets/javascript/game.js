c0082e042d4b6dd0de30c21d8b342810decaeb44
Sarah Doran <sarahdoran@Sarahs-MacBook-Air-2.local>
2016-12-12 23:20:58 -0500

    Here is some more changes. Got the letters to fill in one at a time, but now the correct/ incorrect isn't working again.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,20 +8,44 @@ var words = ["diane", "coffee", "lodge", "truman", "laura"];
 		losses: 0,
 		guessesLeft: 7,
 		letterCounter: 0,
+		gameOver: false,
 	}
 
 //This is the empty array that will hold any guessed letters. 
 	var guessedLetters = [];
 
-// Variable for the words chosen will be chosen randomly out of the array above.
-	var computerWord = words[Math.floor(Math.random() * words.length)];
+	var wordSplit;
 
+	var computerWord;
+
+// This function starts a new game.
+function newGame() {
+
+	// Guesses left are reset, computer chooses a new word and the guessedLetters array becomes empty again. 
+	game.guessesLeft = 7;
+	game.letterCounter = 0;
+	guessedLetters = [];
+	game.gameOver = false;
+	computerWord = newWord();
+
+	function newWord() {
+// Variable for the words chosen will be chosen randomly out of the array above.
+	return words[Math.floor(Math.random() * words.length)];
+	}
+
+//Previous word or dashes will be removed.
+	document.getElementById("hangmanWord").textContent=" ";  
+
+// Guesses are cleared.
+	document.getElementById("guesses").textContent=" ";
+
+// Picture is reset.
+	document.getElementById("picture").innerHTML= "<img src='assets/images/twinpeaks.png'>";
 
 // This variable was set so that each character within the string can be split into individual characters in an array. 
-	var wordSplit = computerWord.split("");
+	wordSplit = computerWord.split("");
 	console.log(wordSplit);
 
-function newWord() {
 // The for loop below will make the letters from the chosen word turn into dashes. 
 	for (var i = 0; i < computerWord.length; i++) {
 
@@ -35,12 +59,11 @@ function newWord() {
 			dashes.textContent = "_";
 
 		// Each letter is replaced in the HTML with dashes, and lastly, appended to the HTML.
-			console.log(document.getElementById("hangmanWord").appendChild(dashes));
+			document.getElementById("hangmanWord").appendChild(dashes);
 	}
 }
 
-// newWord();
-
+newGame();
 // This function will check if a letter has already been guessed.
 
 	function isInArray(a) {
@@ -52,77 +75,91 @@ function newWord() {
     	return false;
 	}
 
+	// This function will check if a letter is a part of the chosen word.
+	function isIncorrect(b) {
+		var check = [], i;
+    	for (var i = 0;i < wordSplit.length; i++) {
+        	if (wordSplit[i] !== b) {
+        		check.push(i)
+   				guessedLetters.push(i);
+			}
+    	}
+    	// return check;
+    	document.getElementById("guesses").textContent += event.key + " ";
+    	game.guessesLeft--;
+	}
+
+// This function will push the correct letter to an array and will therefore, push it to the page.
+
+		function getAllIndexes(val) {
+   		var checker = [], i;
+// This loop will run when a user is trying to guess the hangman word. The word chosen was split into strings, so this became an array.
+   		for (i = 0; i < wordSplit.length; i++)  {
+       		if (wordSplit[i] === val) {
+       			checker.push(i);
+       			guessedLetters.push(i);
+// Because the split word turned into an array, we use the "i" to target the index of each letter within the split word array.
+// When the letter is guessed correctly, the user's guess is written within the split word array.
+				document.getElementById("letter-number" + i).innerHTML = wordSplit[i];
+				game.letterCounter++;
+   			} 
+   		}
+   		return checker;
+
+		}
+
 // This is the code for the reset button. *This is still in need of finishing.
 
-	document.getElementById("restart").onclick = document.getElementById("picture").innerHTML= "<img src='assets/images/twinpeaks.png'>";
+	document.getElementById("restart").onclick = function() {newGame()};
 
 // All of the stuff below will happen when a key is released!
 	document.onkeyup = function (event) {
+		
+			//This will make the userGuess letters the event key and lower case. 
+			var userGuess = event.key.toLowerCase();
 
-		//This will make the userGuess letters the event key and lower case. 
-		var userGuess = event.key.toLowerCase();
+			var matchingSpans = getAllIndexes(userGuess); 
 
-		// This loop will run when a user is trying to guess the hangman word. The word chosen was split into strings, so this became an array.
-		// Because the split word turned into an array, we use the "i" to target the index of each letter within the split word array.
-		// When the letter is guessed correctly, the user's guess is written within the split word array. 
-		for (var i = 0; i < wordSplit.length; i++) {
+			var incorrectLetters = isIncorrect(userGuess);
 
-			if (wordSplit[i] === userGuess) {
-				document.getElementById("letter-number" + i).innerHTML = wordSplit[i];
-				console.log(userGuess);
-				game.letterCounter++;
-			}
-		}
+			// These are the possible user guesses.
+			if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c") || (userGuess === "d") || (userGuess === "e") || (userGuess === "f") || (userGuess === "g") || (userGuess === "h") || (userGuess === "i") || (userGuess === "j") || (userGuess === "k") || (userGuess === "l") || (userGuess === "m") || (userGuess === "n") || (userGuess === "o") || (userGuess === "p") || (userGuess === "q") || (userGuess === "r") || (userGuess === "s") || (userGuess === "t") || (userGuess === "u") || (userGuess === "v") || (userGuess === "w") || (userGuess === "x") || (userGuess === "y") || (userGuess === "z")) {
+ 
 
-		// If the letters guessed do not match the chosen word...
-			if (userGuess !== wordSplit[i]) {
+			// If you guess all of the letters from the chosen word...
+				if (game.letterCounter === wordSplit.length) {
+					alert("You win!");
+					game.wins++;
+					game.gameOver = true;
+					document.getElementById("guesses").textContent = " ";
+				}
+
+				// If the user already guessed a letter.
 				if (isInArray(userGuess)) {
           			console.log("You already guessed that letter!");
        			}
-				game.guessesLeft--;
-				guessedLetters.push(userGuess);
-				document.getElementById("guesses").textContent += event.key + " ";
+
+				// If the user runs out of guesses the following will happen...
+
+				if (game.guessesLeft === 0) {
+					// Audio of the "Fire Walk With Me" poem will play.
+					var audio = new Audio('assets/sound/fwwmpoem.wav');
+					audio.play();
+
+					// The word that was chosen is revealed.
+					alert("You lost. The word was " + computerWord);
+
+					// Bob gif is going to replace the Twin Peaks picture.
+					document.getElementById("picture").innerHTML= "<img src='assets/images/bob.gif'>";
+					
+					game.gameOver = true;
+					game.losses++;
+				}
 			}
 
-			// If you guess all of the letters from the chosen word...
-			if (game.letterCounter === wordSplit.length) {
-				alert("You win!");
+	document.getElementById("wins").innerHTML = game.wins;
+	document.getElementById("losses").innerHTML = game.losses;
+	document.getElementById("guesses-left").innerHTML = game.guessesLeft;
 
-			// Guesses left are reset, computer chooses a new word, and the guessedLetters array becomes empty again. 
-				game.guessesLeft = 7;
-				computerWord = newWord();
-				guessedLetters = [];
-				game.wins++;
-				document.getElementById("hangmanWord").innerHTML = newWord();			
-				document.getElementById("guesses").textContent = " ";
-			}
-
-	// If the user runs out of guesses the following will happen...
-
-		if (game.guessesLeft === 0) {
-			// Audio of the "Fire Walk With Me" poem will play.
-			var audio = new Audio('assets/sound/fwwmpoem.wav');
-			audio.play();
-
-			// Guesses are cleared.
-			document.getElementById("guesses").textContent=" ";
-
-			// The word that was chosen is revealed.
-			alert("You lost. The word was " + computerWord);
-
-			// Bob gif is going to replace the Twin Peaks picture.
-			document.getElementById("picture").innerHTML= "<img src='assets/images/bob.gif'>";
-
-			//This will generate a new word. 
-			document.getElementById("hangmanWord").innerHTML = newWord();			
-			
-			// Guesses left are reset, computer chooses a new word, and the guessedLetters array becomes empty again. 
-			game.guessesLeft = 7;
-			guessedLetters = [];
-			game.losses++;
-		}
-
-		document.getElementById("wins").innerHTML = game.wins;
-		document.getElementById("losses").innerHTML = game.losses;
-		document.getElementById("guesses-left").innerHTML = game.guessesLeft;
 	}
+	
